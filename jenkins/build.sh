785eedf719116921a9b70ea34eab9d997dce4adb
Ubuntu <mmalik@WebApp.t4304vjl1x0etms5apdej45zyh.cwx.internal.cloudapp.net>
2021-12-30 15:10:35 +0000

    running docker, build and setup updated

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "build stage"
+
+
+
+#building images
+docker-compose build
--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
 echo "setup stage"
+
+#apt
+sudo apt-get update
+sudo apt-get install -y curl jq
+#installing docker
+
+curl https://get.docker.com | sudo bash
+
+#install docker compose
+
+
+version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+#docker login
+docker login --username $DOCKER_HUB_CREDS_USR --password $DOCKER_HUB__CREDS_PSW
