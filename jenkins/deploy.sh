f9c4c92cc90b355ba83a9818b5ff8767bdea7a2f
Ubuntu <mmalik@WebApp.t4304vjl1x0etms5apdej45zyh.cwx.internal.cloudapp.net>
2021-12-31 18:45:36 +0000

    deploy stage

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 echo "deploy stage"
+
+scp docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
+ssh jenkins@swarm-manager docker stack deploy --compose-file docker-compose.yaml football-app
