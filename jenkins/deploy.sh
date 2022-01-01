8349f75042595f7992b8fbbe43661697d5754bc4
Ubuntu <mmalik@WebApp.t4304vjl1x0etms5apdej45zyh.cwx.internal.cloudapp.net>
2022-01-01 15:16:29 +0000

    latest

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -3,4 +3,4 @@
 echo "deploy stage"
 
 scp docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
-ssh jenkins@swarm-manager docker stack deploy --compose-file docker-compose.yaml football-app
+ssh jenkins@swarm-manager DATABASE_URI=$DATABASE_URI docker stack deploy --compose-file docker-compose.yaml football-app
